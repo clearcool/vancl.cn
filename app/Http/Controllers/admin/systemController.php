9fc93c3a3d3dfed36e537d89cc737bbb83387bdf
张振 <978054200@qq.com>
2017-03-29 19:58:48 +0800

    0.1.2

--- app/Http/Controllers/admin/systemController.php
+++ app/Http/Controllers/admin/systemController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\admin;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+use DB;
 
 
 
@@ -17,38 +17,12 @@ class systemController extends Controller
 {
 
 	//基本设置
-    public function getSystemBase(Request $request)
+    public function getSystembase()
     {
+        $config = DB::table('config')->get();
+
+        dd($config);
         return view('admin.system.system-base');
     }
 
-    //栏目管理
-    public function getSystemCategory(Request $request)
-    {
-        return view('admin.system.system-category');
-    }
-
-    //栏目设置
-    public function getSystemCategoryAdd(Request $request)
-    {
-        return view('admin.system.system-category-add');
-    }
-
-    //数据字典
-    public function getSystemData(Request $request)
-    {
-        return view('admin.system.system-data');
-    }
-
-    //系统日志
-    public function getSystemLog(Request $request)
-    {
-        return view('admin.system.system-log');
-    }
-
-    //基本设置
-    public function getSystemShielding(Request $request)
-    {
-        return view('admin.system.system-shielding');
-    }
 }
